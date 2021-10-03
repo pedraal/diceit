a62bf02ee0ea20d316af7e217c1d314e56768d82
Pierre Golfier <pgolfier.pro@gmail.com>
2021-10-03 16:32:11 +0200

    fix 404 pages

---
+++ src/pages/[...all].vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { isDark } from '~/logic'
+</script>
+
+<template>
+  <div class="py-6 sm:py-8 lg:py-12">
+    <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+      <div class="flex flex-col items-center">
+        <p class="font-semibold text-sm text-secondary mb-4 uppercase dark:text-primary">
+          That’s a 404
+        </p>
+        <h1 class="font-bold text-center mb-2 text-2xl md:text-3xl">
+          Page not found
+        </h1>
+
+        <p class="max-w-screen-md text-center text-muted mb-12 md:text-lg">
+          The page you’re looking for doesn’t exist.
+        </p>
+
+        <router-link to="/">
+          <Button :variant="isDark ? 'primary' : 'secondary'">
+            Go home
+          </Button>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
